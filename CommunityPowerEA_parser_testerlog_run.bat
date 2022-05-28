d829986d2ddcd0f7795eef1fa19fbef7cf5a01df
ulises2k <ulises2k@gmail.com>
2022-05-28 12:52:22 -0300

    New Parameter

--- CommunityPowerEA_parser_testerlog_run.bat
+++ CommunityPowerEA_parser_testerlog_run.bat
@@ -1,3 +1,3 @@
-set DATENOW=20210808
-c:\Python38\python.exe CommunityPowerEA_parser_testerlog.py > %DATENOW%.csv
+set DATENOW=20220209
+c:\Python38\python.exe CommunityPowerEA_parser_testerlog.py -mt5_visual_mode_checked off > %DATENOW%.csv 
 c:\Python38\python.exe CommunityPowerEA_convert_CSV_To_Excel.py -i %DATENOW%.csv -o %DATENOW%.xlsx
